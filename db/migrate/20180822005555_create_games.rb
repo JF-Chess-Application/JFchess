dc1d1e546ba96543770a7e4d2c34cfb4e78c1ae6
knoll3 <34484576+knoll3@users.noreply.github.com>
2018-08-21 21:24:27 -0500

    Update 20180822005555_create_games.rb
    Change player_one and player_two to integers and add index.

--- db/migrate/20180822005555_create_games.rb
+++ db/migrate/20180822005555_create_games.rb
@@ -3,12 +3,14 @@ class CreateGames < ActiveRecord::Migration[5.0]
     create_table :games do |t|
       t.integer :user_id
       t.date :end_date
-      t.string :player_one
-      t.string :player_two
+      t.integer :player_one
+      t.integer :player_two
       t.string :winner
 
       t.timestamps
     end
     add_index :games, :user_id
+    add_index :games, :player_one
+    add_index :games, :player_two
   end
 end
