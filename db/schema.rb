eb6dd4e6a6c94c9645b38e5a98a0851f8626e17b
Brittany Rodriguez <brittanyr303@gmail.com>
2018-08-22 04:58:22 +0000

    edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180815041759) do
+ActiveRecord::Schema.define(version: 20180822044942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -31,4 +36,5 @@ ActiveRecord::Schema.define(version: 20180815041759) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
+
 end
