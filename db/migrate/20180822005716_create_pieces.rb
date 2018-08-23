c1adf052cd24b9a2d304f7f102d0255259123ce7
knoll3 <34484576+knoll3@users.noreply.github.com>
2018-08-22 21:48:48 -0500

    Update 20180822005716_create_pieces.rb
    Changed to `position_x` and `position_y`

--- db/migrate/20180822005716_create_pieces.rb
+++ db/migrate/20180822005716_create_pieces.rb
@@ -4,10 +4,8 @@ class CreatePieces < ActiveRecord::Migration[5.0]
       t.string :name
       t.integer :game_id
       t.integer :user_id
-      t.string :moved_from_x
-      t.string :moved_from_y
-      t.string :moved_to_x
-      t.string :moved_to_y
+      t.string :position_x
+      t.string :position_y
 
       t.timestamps
     end
