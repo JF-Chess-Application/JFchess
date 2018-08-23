b2a70f25e4a354161696c9e4bcfba9f8eb69a0ef
Knoll <noll.kyle@gmail.com>
2018-08-22 23:22:20 -0500

    First db_migration

---
+++ db/migrate/20180823041559_create_games.rb
@@ -0,0 +1,13 @@
+class CreateGames < ActiveRecord::Migration[5.0]
+  def change
+    create_table :games do |t|
+      t.integer :user_id
+      t.date :end_date
+      t.integer :opponent
+      t.integer :winner
+
+      t.timestamps
+    end
+    add_index :games, :user_id
+  end
+end
