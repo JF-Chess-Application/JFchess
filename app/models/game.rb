ea14fb662004b3b6f3baef3338007204c8029e51
Amy Henning <henning.amy@gmail.com>
2018-08-24 03:57:46 +0000

    Added is_obstructed? method to piece model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,9 @@
 class Game < ApplicationRecord
   belongs_to :user
   has_many :pieces
+
+  # define a method to check if a piece is already on a specific space
+  def piece_in_space?(x, y)
+    pieces.where(position_x: x, position_y: y).any? ? true : false
+  end
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,28 @@
 class Piece < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  # use the current x_position and y_position to check if spaces are occupied
+  def is_obstructed?(position_x, position_y)
+    forward_one = position_y + 1
+    backward_one = position_y - 1
+    right_one = position_x + 1
+    left_one = position_x - 1
+  # deal with invalid cases first - moves that would put the piece off the board (operating with 0 index for x and y)
+    if right_one > 7 || left_one < 0 || forward_one > 7 || backward_one < 0
+      return true
+  # deal with horizontal movement
+    elsif game.piece_in_space?(right_one, position_y) || game.piece_in_space?(left_one, position_y)
+      return true
+  # deal with vertical movement
+    elsif game.piece_in_space?(position_x, forward_one) || game.piece_in_space?(position_x, backward_one)
+      return true
+  # deal with diagonal movement
+    elsif game.piece_in_space?(right_one, forward_one) || game.piece_in_space?(right_one, backward_one) || game.piece_in_space?(left_one, forward_one) || game.piece_in_space?(left_one, backward_one)
+      return true
+  # if none of those is true, return false
+    else
+      return false
+    end
+  end
 end
