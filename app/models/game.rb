ecf1926a0487c60e5f833f0f53758bae31e4ab44
Amy Henning <henning.amy@gmail.com>
2018-08-30 02:16:47 +0000

    Added available scope

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,9 @@ class Game < ApplicationRecord
   belongs_to :user
   has_many :pieces
 
+  # define available scope
+  scope :available, -> { where(opponent: nil) }
+
   # define a method to check if a piece is already on a specific space
   def piece_in_space?(x, y)
     pieces.where(position_x: x, position_y: y).any? ? true : false
