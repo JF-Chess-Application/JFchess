8901742ad76ee8633ed7f9920c8c7efb187a517c
amyhenning <henning.amy@gmail.com>
2018-09-06 03:17:30 +0000

    Set up valid_move? for Knight subclass

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,23 @@
 class Knight < Piece
+	def valid_move?(x, y)
+		# check validity based on board position - moves off board are invalid. This logic is built into is_obstructed but we aren't calling that method in this subclass
+		if x > 7 || x < 0 || y > 7 || y < 0
+			return false
+		# l-shapes upward
+		elsif y == position_y + 2 && (x == position_x + 1 || x == position_x - 1)
+			return true
+		# l-shapes to the right
+		elsif x == position_x + 2 && (y == position_y + 1 || y == position_y - 1)
+			return true
+		# l-shapes downward
+		elsif y == position_y - 2 && (x == position_x - 1 || x == position_x + 1)
+			return true
+		# l-shapes to the left
+		elsif x == position_y - 2 && (x == position_x + 1 || x == position_x - 1)
+			return true
+		# all other cases
+		else
+			return false
+		end
+	end
 end
