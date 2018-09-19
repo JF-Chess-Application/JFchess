8dfd870dad1ceedd88c60fb5d541896658803fd0
Knoll <noll.kyle@gmail.com>
2018-09-18 22:25:11 -0500

    valid_move? for Rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,2 +1,17 @@
 class Rook < Piece
+  def valid_move?(x, y)
+    
+    # If move is horizontal and not obstructed
+    if position_y == y && !self.is_obstructed?(x, y)
+      return true
+    end 
+
+    # If move is vertical and not obstructed
+    if position_x == x && !self.is_obstructed?(x, y)
+      return true
+    end
+
+    # All other casees
+    return false
+  end
 end
