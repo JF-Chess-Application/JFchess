76139c1f8d54b436ac9c28f409d760fc76e9089b
Justin Cefai <cefaijustin@gmail.com>
2018-09-21 01:43:33 +0000

    updated pieces controller with turn based logic

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -7,9 +7,21 @@ class PiecesController < ApplicationController
 		@piece = Piece.find(params[:id])
 		position_x = params[:position_x]
 		position_y = params[:position_y]
-		@piece.update_attributes(position_x: position_x, position_y: position_y)
-		
-		@game = Game.find(@piece.game_id)
-		redirect_to game_path(game)
+
+		if !current_user_turn 
+			render text: 'Please wait your turn',
+			status: :unauthorized
+		else 
+			@piece.update_attributes(position_x: position_x, position_y: position_y)
+			
+			@game = Game.find(@piece.game_id)
+			redirect_to game_path(game)
+		end
+	end
+
+	private 
+
+	def current_user_turn?
+		@game.turn == current_user.id
 	end
 end
