4914192b943100ce0142d4cdbbc4cbacfc3ec4dd
amyhenning <henning.amy@gmail.com>
2018-09-23 00:32:55 +0000

    Made progress on check? helper methods and updated king valid_move? for some tests

--- app/models/game.rb
+++ app/models/game.rb
@@ -49,7 +49,7 @@ class Game < ApplicationRecord
     pieces.where(position_x: x, position_y: y).any? ? true : false
   end
 
-  # set methods to define the kings per DRY conventions
+  # set methods to define the kings and pieces of each color per DRY conventions
   def black_king
     King.find_by(game_id: id, type: 'King', color: 'black')
   end
@@ -57,14 +57,22 @@ class Game < ApplicationRecord
   def white_king
     King.find_by(game_id: id, type: 'King', color: 'white')
   end
+
+  def black_pieces
+    Piece.where(game_id: id, color: 'black')
+  end
+
+  def white_pieces
+    Piece.where(game_id: id, color: 'white')
+  end
   
   # helper method for threatened_king?
   def white_king_threatened?
     white_king_x = white_king.position_x
     white_king_y = white_king.position_y
     # true if a black piece has a valid_move?(white_king_x, white_king_y)
-    self.pieces.each do |piece|
-      if piece.color == 'black' && piece.valid_move?(white_king_x, white_king_y)
+    black_pieces.each do |piece|
+      if piece.valid_move?(white_king_x, white_king_y)
         return true
       end
     end
@@ -76,15 +84,15 @@ class Game < ApplicationRecord
     black_king_x = black_king.position_x
     black_king_y = black_king.position_y
     # true if a white piece has a valid_move?(black_king_x, black_king_y)
-    self.pieces.each do |piece|
-      if piece.color == 'white' && piece.valid_move?(black_king_x, black_king_y)
+    white_pieces.each do |piece|
+      if piece.valid_move?(black_king_x, black_king_y)
         return true
       end
     end
     return false
   end
 
-  # in_check? CONDITION 1: CHECK FOR THREATENED KINGS
+  # check? CONDITION 1: CHECK FOR THREATENED KINGS
   def threatened_king?
     # if there is any piece whose valid_move? method returns true to a space with a King in it, return true
     if white_king_threatened? || black_king_threatened?
@@ -94,21 +102,17 @@ class Game < ApplicationRecord
     end
   end
 
-  # in_check? CONDITION 2: THREATENED KING CAN MOVE
+  # check? CONDITION 2: THREATENED KING CAN MOVE
   # The threatened king can move into a different space where no pieces are currently able to capture it
   def threatened_king_can_move?(x, y)
     # these methods should check (1) that the king can make a move to a given x, y AND check each piece from the opposing player. If the king can make the move and an opponent's piece ALSO has a valid_move? to that space, the method should return false. 
     # if the king CAN make a valid_move? to the desired x, y AND there is no opponent's piece with a valid_move? to that space, the method should return true
-
-
-    # three arrays - all the places the king can move to, all the places where the opponent's pieces can move, all the places wher eyour pieces (except the king) can move to
-      # if the king can move to a spot that's not included in the opponent's pieces
     if white_king_threatened?
       if white_king.valid_move?(x, y)
         king_can_move = false
-        self.pieces.each do |piece|
+        black_pieces.each do |piece|
           next if king_can_move
-          if piece.color == 'black' && piece.valid_move?(x, y)
+          if piece.valid_move?(x, y)
             king_can_move = false
           else
             king_can_move = true
@@ -120,68 +124,75 @@ class Game < ApplicationRecord
 
     if black_king_threatened?
       if black_king.valid_move?(x, y)
-        puts "the king can move there"
-        king_can_move = true
-        self.pieces.each do |piece|
-          next if king_can_move
-          if piece.color == 'white' && piece.valid_move?(x, y)
+        king_can_move = false
+        white_pieces.each do |piece|
+          # next if king_can_move
+          if piece.valid_move?(x, y)
             king_can_move = false
-            puts "the king can't move there"
-            return king_can_move
+            # return king_can_move
           else
             king_can_move = true
           end
         end
+        return king_can_move
       end
-      return king_can_move
+    end
+  end
+
+# the piece or pieces that have valid_moves to the king's space
+  def threats
+    if white_king_threatened?
+      black_threats = []
+      black_pieces.each do |piece|
+        if piece.valid_move?(white_king.position_x, white_king.position_y)
+          black_threats.push(piece)
+        end
+      end
+      return black_threats
     end
 
-    # if black_king_threatened?
-    #   if black_king.valid_move?(x, y)
-    #     self.pieces.each do |piece|
-    #       if piece.color == 'white' && piece.valid_move?(x, y)
-    #         puts "there is a piece threatening the king"
+    if black_king_threatened?
+      white_threats = []
+      white_pieces.each do |piece|
+        if piece.valid_move?(black_king.position_x, black_king.position_y)
+          white_threats.push(piece)
+        end
+      end
+      return white_threats
+    end
+  end
+
+  # check? CONDITION 3: THE PIECE THREATENING THE KING CAN BE CAPTURED
+  # The piece threatening the king can be captured
+  def threat_is_capturable?
+    # if white_king_threatened?
+    #   threats.each do |threat|
+    #     black_pieces.each do |piece|
+    #       if piece.valid_move?(threat.position_x, threat.position_y)
+    #         return true
     #       end
-    #       return true
+    #       return false
     #     end
     #   end
     # end
+
+    if black_king_threatened?
+      can_capture_threat = false
+      white_pieces.each do |piece|
+        threats.each do |threat|
+          next if can_capture_threat
+          if piece.valid_move?(threat.position_x, threat.position_y)
+            can_capture_threat = true
+          else
+            can_capture_threat = false
+          end
+        end
+      end
+      return can_capture_threat
+    end
   end
 
-
-  # new class + new object to store in memory the current state of the game's next move
-  # maybe new class called move_manager, instance variables - attr_accessors keep track of threats
-  # initialize - board, pieces threatening the king, 
-  # can store that instance vars in this class - can then see new ways to move it out of this class
-  # functional vs. persisting in memory - OO way is that the threat can store in memory; every time you make a move, you would have an array/variable that you update (pieces_threatening_king) - would always have a list of pieces threatening the king
-  # OO - state and behavior - call a functional method somewhere else, result of functional method is stored in memory, so the next time it's called
-  # functional - takes an input and gives an output (math), calculator, + is a method, doesn't care about what has happened in the past
-  def threat(king)
-    # the piece or pieces that have valid_moves to the king's space
-    # need to figure out a way to define the threatening piece
-    # should also return the piece's color
-    # compute all possible moves for the pieces, do any of those overlap for the king's current place
-
-    # if white king, look at black pieces' potential moves
-      # do any of the moves have a valid_move?(x, y) where that's the king's position
-
-    # 
-  end
-
-  # helper method for threat_is_capturable?
-  def is_capturable?
-    # if the threatening piece is white, check if any black pieces have a valid_move? to its spot
-
-    # if the threatening piece is black, check if any white pieces have a valid_move? to its spot
-  end
-
-  # in_check? CONDITION 3: THE PIECE THREATENING THE KING CAN BE CAPTURED
-  # The piece threatening the king can be captured without causing another piece to threaten the king
-  def threat_is_capturable?
-
-  end
-
-  # in_check? CONDITION 4: CAN OBSTRUCT THE THREATENING PIECE'S MOVE
+  # check? CONDITION 4: CAN OBSTRUCT THE THREATENING PIECE'S MOVE
   # There is a piece of the same color as the threatened king that can move between the threat and the king without causing another piece to threaten the king
   def threat_is_obstructable?
 
--- app/models/king.rb
+++ app/models/king.rb
@@ -1,22 +1,50 @@
 class King < Piece
+  # def valid_move?(x, y)
+  #   if self.is_obstructed?(x, y)
+  #     return false
+  #   end
+
+    # up_one = position_y + 1
+    # down_one = position_y - 1
+    # left_one = position_x - 1
+    # right_one = position_x + 1
+
+    # # check if it's a valid horizontal move
+    # if y == position_y && (x == right_one || x == left_one)
+    #   return true
+    # # check if it's a valid vertical move
+    # elsif x == position_x && (y == up_one || y == down_one)
+    #   return true
+    # # check if it's a valid diagonal move
+    # elsif (x == right_one && y == up_one) || (x == left_one && y == up_one) || (x == left_one && y == down_one) || (x == right_one && y == down_one)
+    #   return true
+    # # # check if x is valid but not y
+    # # elsif (x == position_x || x == right_one || x == left_one) && (y != position_y || y != up_one || y != down_one)
+    # #   return false
+    # # # check if y is valid but not x
+    # # elsif (x != position_x || x != right_one || x != left_one) && (y == position_y || y == up_one || y == down_one)
+    # #   return false
+    # # # check if both x and y are invalid
+    # # elsif (x != position_x || x != right_one || x != left_one) && (y != position_y || y != up_one || y != down_one)
+    # #   return false
+    # else
+    #   return false
+    # end
+
+
   def valid_move?(x, y)
-
-    if self.is_obstructed?(x, y)
-      return false
+    return false if is_obstructed?(x, y)
+    if moves_only_one_space?(x, y)
+      return true
     end
+    return false
+  end
 
-    # check if it's a valid horizontal move
-    if y == position_y && (x == position_x + 1 || x == position_x - 1)
+  def moves_only_one_space?(x, y)
+    return false if position_x == x && position_y == y
+    if (position_x - x).abs <= 1 && (position_y - y).abs <= 1
       return true
-    # check if it's a valid vertical move
-    elsif x == position_x && (y == position_y + 1 || y == position_y - 1)
-      return true
-    # check if it's a valid diagonal move
-    elsif (x == position_x + 1 && y == position_y + 1) || (x == position_x - 1 && y == position_y + 1) || (x == position_x - 1 && y == position_y - 1) || (x == position_x + 1 && y == position_y - 1)
-      return true
-    # if none of the above, false
-    else
-      return false
     end
+    return false
   end
 end
