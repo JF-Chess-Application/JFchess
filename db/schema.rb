87f9ffe873934ba55d6789d95eee6557ccba319f
Justin Cefai <cefaijustin@gmail.com>
2018-09-24 19:26:48 +0000

    updated games database table to include turns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180911041340) do
+ActiveRecord::Schema.define(version: 20180924192445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180911041340) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.integer  "turns"
     t.index ["user_id"], name: "index_games_on_user_id", using: :btree
   end
 
@@ -37,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180911041340) do
     t.string   "status"
     t.integer  "move_count", default: 0
     t.string   "color"
+    t.string   "type"
     t.index ["game_id"], name: "index_pieces_on_game_id", using: :btree
     t.index ["user_id"], name: "index_pieces_on_user_id", using: :btree
   end
