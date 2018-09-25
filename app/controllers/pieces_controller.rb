5adc72a2630662f55c0785c7204b6eafb6f2951a
amyhenning <henning.amy@gmail.com>
2018-09-25 03:11:46 +0000

    Set up some additional logic for displaying an alert

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -19,8 +19,9 @@ class PiecesController < ApplicationController
     	new_move_count = @piece.move_count + 1
     	@piece.update_attributes(move_count: new_move_count)
 		else
-			# @game = Game.find(@piece.game_id)
-			# redirect_to game_path(@game), alert: 'Invalid move. Please try another move.'
+			@game = Game.find(@piece.game_id)
+			byebug
+			redirect_to game_path(@game), alert: 'Invalid move'
 			return
 		end
 	end
