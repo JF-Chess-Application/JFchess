8398312b5c8cea087dcfaafde8ef10944799fdaf
amyhenning <henning.amy@gmail.com>
2018-09-25 02:19:45 +0000

    Updated rook and knight valid_move methods and moved move_count incrementing logic to pieces controller

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -15,9 +15,12 @@ class PiecesController < ApplicationController
 		target_y = params[:position_y]
 		if @piece.valid_move?(target_x, target_y)
 			@piece.update_attributes(position_x: target_x, position_y: target_y)
+    	# update the move_count
+    	new_move_count = @piece.move_count + 1
+    	@piece.update_attributes(move_count: new_move_count)
 		else
-			@game = Game.find(params[:game_id])
-			redirect_to game_path(@game), alert: 'Invalid move'
+			@game = Game.find(@piece.game_id)
+			redirect_to game_path(@game), alert: 'Invalid move. Please try another move.'
 			return
 		end
 	end
